0a28a4a43030a2071089d205006676b4715eebf3
johnodon <johnodon@hotmail.com>
2016-02-10 17:50:53 -0500

    Update install.sh

--- files/install.sh
+++ files/install.sh
@@ -3,20 +3,12 @@
 #Gourmet
 
 #####################################
-#	Install dependencies		        	#
+#	Install dependencies and app    	#
 #							                  		#
 #####################################
 
 apt-get update -qq
 apt-get install -qy gourmet
-
-#####################################
-#	Fix keyboard mappings rdp	      	#
-#						                  			#
-#####################################
-sed -i.bak '/[default_rdp_layouts]/ a rdp_layout_no=0x00000414' /etc/xrdp/xrdp_keyboard.ini
-sed -i.bak '/[default_layouts_map]/ a rdp_layout_no=no' /etc/xrdp/xrdp_keyboard.ini
-cp /files/keymaps/*.ini /etc/xrdp/
 
 #####################################
 #	Add configs and needed stuff    	#
